560da7b400212a15a1c28dbac58585a4bf21f56b
zhaofelix <15021179915@163.com>
2022-03-15 20:30:35 +0800

    冒泡排序完毕

--- AlgorithmsInSwift.playground/Pages/BubbleSort.xcplaygroundpage/Contents.swift
+++ AlgorithmsInSwift.playground/Pages/BubbleSort.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 
 public func bubbleSort<Element>(_ array: inout [Element]) where Element: Comparable {
+// 元素数量少于两个无需进行排序
     guard array.count >= 2 else {
         return
     }
@@ -16,3 +17,9 @@ public func bubbleSort<Element>(_ array: inout [Element]) where Element: Compara
         }
     }
 }
+
+/*:
+ 冒泡排序的时间复杂度：
+ - 最好的表现是O(n)，在数组已经排好序的情况下。
+ - 最差的表现是O(n²)。
+*/
--- AlgorithmsInSwift.playground/Pages/Main.xcplaygroundpage/Contents.swift
+++ AlgorithmsInSwift.playground/Pages/Main.xcplaygroundpage/Contents.swift
@@ -1,4 +1,8 @@
 var array = [9, 2, 3, 5, 7]
+// 类似于指针
 bubbleSort(&array)
 print(array)
 
+for i in (1..<array.count).reversed() {
+    print(i)
+}
